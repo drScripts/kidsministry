19832b59558e39696a144b4549922f7529eaa01b
Nathanael <nathanael.vd@gmail.com>
2021-06-18 01:25:08 +0700

    Menambahkan Blocking Inspect Element

--- app/Views/template/header.php
+++ app/Views/template/header.php
@@ -16,7 +16,8 @@
 
 
 <!DOCTYPE html>
-<html lang="en"> 
+<html lang="en">
+
 <head>
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -34,15 +35,15 @@
   <link href="<?= base_url(); ?>/assets/css/black-dashboard.min.css?v=1.0.0" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
   <link href="<?= base_url(); ?>/assets/demo/demo.css" rel="stylesheet" />
-  <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/my-css.css"> 
+  <link rel="stylesheet" href="<?= base_url(); ?>/assets/css/my-css.css">
   <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
-  
+
   <script src="<?= base_url(); ?>/assets/js/core/jquery.min.js"></script>
   <script src="<?= base_url(); ?>/assets/js/core/popper.min.js"></script>
   <!--   Core JS Files   -->
   <script src="<?= base_url(); ?>/assets/js/core/bootstrap.min.js"></script>
   <script src="<?= base_url(); ?>/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
-  <!--  Google Maps Plugin    --> 
+  <!--  Google Maps Plugin    -->
   <!-- Chart JS -->
   <script src="<?= base_url(); ?>/assets/js/plugins/chartjs.min.js"></script>
   <!--  Notifications Plugin    -->
@@ -53,6 +54,32 @@
   <script src="https://kit.fontawesome.com/01059e4e8b.js" crossorigin="anonymous"></script>
   <!-- sweet alert 2 -->
   <script src="//cdn.jsdelivr.net/npm/sweetalert2@10"></script>
+
+  <!-- blocking inspect -->
+  <!-- <script>
+    // Disable inspect element
+    $(document).bind("contextmenu", function(e) {
+      e.preventDefault();
+    });
+    $(document).keydown(function(e) {
+      if (e.which === 123) {
+        return false;
+      }
+
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'I'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'C'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.shiftKey && e.keyCode == 'J'.charCodeAt(0)) {
+        return false;
+      }
+      if (e.ctrlKey && e.keyCode == 'U'.charCodeAt(0)) {
+        return false;
+      }
+    });
+  </script> -->
 </head>
 
-<body class="">  
+<body class="">
